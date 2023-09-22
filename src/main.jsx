903920e6794a8d6be5dbb5fa17787cfd1b29f977
Beatriz <beatrizrmb@outlook.com>
2023-09-22 12:05:45 +0100

    Fix import BrowserRouter as Router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthProviderWrapper } from './context/auth.context.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
       <AuthProviderWrapper>
+  <React.StrictMode>
+    <Router>
         <App />
-      </AuthProviderWrapper>
+      </Router>
   </React.StrictMode>
+      </AuthProviderWrapper>
 )
