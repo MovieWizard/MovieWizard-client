4eeeb1a64b577c2b13524bc65a65b0c88d746c93
Merna <merna.wahdan@gmail.com>
2023-09-22 13:33:56 +0200

    Add NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@
 import { Routes, Route } from 'react-router-dom'
 
 import './App.css'
+
 import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
 //import IsPrivate from './components/IsPrivate';
 import IsAnon from './components/IsAnon';
 import CreateMovie from './pages/CreateMovie'
 import HomePage from './pages/HomePage'
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className='App'>
-      
+      <NavBar />
         <Routes>
           <Route path="/" element={<HomePage/>} />
           <Route path="/create-movie" element={<CreateMovie/>}/>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,14 +2,16 @@ import {NavLink} from 'react-router-dom'
 function NavBar() {
   return (
     <>
+    <div className='nav-bar'>
       <nav>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/movies">Suggestions</NavLink>
         <NavLink to="/create-movie">Create a Movie</NavLink>
-        <NavLink to="/mood-lists">Mood Lists</NavLink>
+        <NavLink className="sign" to="/mood-lists">Mood Lists</NavLink>
         <NavLink to="/login">Login</NavLink>
         <NavLink to="/signup">Sign up</NavLink>
       </nav>
+    </div>
     </>
   );
 }
