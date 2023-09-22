6cc7d62115702fb8e07b69c045682e161e775a31
Merna <merna.wahdan@gmail.com>
2023-09-22 13:13:03 +0200

    Add NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,17 @@
-Beatriz:
+import {NavLink} from 'react-router-dom'
+function NavBar() {
+  return (
+    <>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Suggestions</NavLink>
+        <NavLink to="/create-movie">Create a Movie</NavLink>
+        <NavLink to="/mood-lists">Mood Lists</NavLink>
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/signup">Sign up</NavLink>
+      </nav>
+    </>
+  );
+}
 
-<AddMovie>
-    
-</AddMovie>
+export default NavBar;
