bb1bcd377a58467bc17f3c24dc45cdf1f5c6d0b2
Merna <merna.wahdan@gmail.com>
2023-09-23 12:06:17 +0200

    Add navigate function to movie details

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useEffect, useState } from "react"
 import axios from "axios"
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 
 
 
 const apiUrl =  "http://localhost:5005"
 function SearchResults () {
 
-    const [search, setSearch] = useState(null)
+    const [search, setSearch] = useState([])
     const [searchParams] = useSearchParams(window.location.search);
 
     const searchParamResult = searchParams.get("query")
@@ -17,16 +17,29 @@ function SearchResults () {
         axios.get(`${apiUrl}/api/search?q=${searchParamResult}`)
         .then(res => {
             setSearch(res.data)
-            console.log(res)
+            console.log('im here', res)
         
         })
         .catch(e => console.log("error to get search results", e))
 
     }, [])
+    const navigate = useNavigate()
 
-    return (
-        <h1>Movies</h1>
-    )
+    const handleClick = (movie) => {
+        navigate(`/movies/${movie._id}`)
+    }
+//I will need here then to show the search results, maybe includes method
+
+const SearchResults = () => {
+    if(SearchResults === null) {
+        return <p>Loading Results...</p>
+    }
+
+    //TODO add movie component
+
 }
+    
+}
+
 
 export default SearchResults
