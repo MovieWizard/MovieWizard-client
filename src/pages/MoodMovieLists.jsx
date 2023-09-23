68ea4abc6bede184b7d21af80160cf88c16cfda3
Merna <merna.wahdan@gmail.com>
2023-09-23 23:10:09 +0200

    Add modal to mood movie lists page

--- src/pages/MoodMovieLists.jsx
+++ src/pages/MoodMovieLists.jsx
@@ -1,62 +1,84 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios"
+import axios from "axios";
+import Modal from "../components/Modal";
 
-const apiUrl =  "http://localhost:5005"
+const apiUrl = "http://localhost:5005";
 
 function MoodMovieLists() {
+  const storedToken = localStorage.getItem("authToken");
 
-    const storedToken = localStorage.getItem("authToken");
+  const [lists, setLists] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
-    const [lists, setLists] = useState([])
 
-    useEffect(() => {
-        axios.get(`${apiUrl}/api/mood-lists`,
-        {headers: { Authorization: `Bearer ${storedToken}`}})
-        .then(response => {
-            setLists(response.data)
-        })
-        .catch(e => {
-            response.status(500).json({
-                message: "Error get all lists",
-                error: e
-            }) 
-        })
-    }, [])
+  useEffect(() => {
+    axios
+      .get(`${apiUrl}/api/mood-lists`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => {
+        setLists(response.data);
+      })
+      .catch((e) => {
+        response.status(500).json({
+          message: "Error get all lists",
+          error: e,
+        });
+      });
+  }, []);
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const handleClick = (list) => {
-        navigate(`/mood-lists/${list._id}`)
+  const handleClick = (list) => {
+    navigate(`/mood-lists/${list._id}`);
+  };
+
+
+
+const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    navigate(`/mood-lists`)  
+
+  };
+
+  const moodLists = () => {
+    if (moodLists === null) {
+      return <p>Mood Lists are Loading...</p>;
     }
 
-    const moodLists = () => {
-        if(moodLists === null) {
-            return <p>Mood Lists are Loading...</p>
-        }
-        // console.log("lissssst", lists);
-        
-        return lists.map(e => {
-            // console.log("eeeeeee", e);
+    return lists.map((e) => {
+      return (
+        <section key={e._id} className="card" onClick={() => handleClick(e)}>
+          <p>{e.title}</p>
+        </section>
+      );
+    });
+  };
+  console.log("moodlists", moodLists());
+  return (
+    <>
+      {moodLists()}
 
-            return (
-                <section key={e._id} className="card" onClick={() => handleClick(e)}>
 
-                <p>{e.title}</p>
-                </section>
+      <Modal
+        showModal={showModal}
+        handleCloseModal={handleCloseModal}
+      >
 
-            )
+      <p className="modal-font-color">Create Mood list</p>
+      <button type="submit" onClick={handleCloseModal}>Create</button>
 
-        })
-    }
-console.log("moodlists", moodLists());
-    return (
-<>
-{moodLists()}
-<button>Add new Mood List</button>
 
-</>
-    )
+      </Modal>
+      <button onClick={handleOpenModal} >Add new Mood List</button>
+
+    </>
+  );
 }
 
-export default MoodMovieLists
+export default MoodMovieLists;
