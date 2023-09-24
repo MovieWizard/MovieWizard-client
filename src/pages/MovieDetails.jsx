d05527391e5d55d51ecf476f739e80754c2b445e
Beatriz <beatrizrmb@outlook.com>
2023-09-24 19:50:55 +0100

    Fixed movie details that weren't showing up

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -3,32 +3,34 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function MovieDetails() {
-    const { movieId } = useParams();
-    
-    const [movieDetails, setMovieDetails] = useState({});
+  const { movieId } = useParams();
 
-    useEffect(() => {
-        axios.get(`http://localhost:5005/api/movies/${movieId}`)
-            .then(response => {
-                console.log(response.data)
-                setMovieDetails(response.data)
+  const [movieDetails, setMovieDetails] = useState({});
 
-            })
-            .catch( e => {
-                console.log('Error getting movie details from the API', e)
-            })
-    }, [movieId])
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5005/api/movies/${movieId}`)
+      .then((response) => {
+        console.log(response.data);
+        setMovieDetails(response.data);
+      })
+      .catch((e) => {
+        console.log("Error getting movie details from the API", e);
+      });
+  }, [movieId]);
 
-    return (
-        <>
-        <h1>{movieDetails.title}</h1>
-        <img src={movieDetails.poster} alt="movie-image" />
-        <h3>Year: {movieDetails.year} | Rating: {movieDetails.imdbRating} | Language: {movieDetails.language} | Genre: {movieDetails.genre}</h3>
-        <h4>Cast: {movieDetails.actors}</h4>
-        <p>Plot: {movieDetails.plot}</p>
-
-        </>
-    )
+  return (
+    <>
+      <h1>{movieDetails.Title}</h1>
+      <img src={movieDetails.Poster} alt="movie-image" />
+      <h3>
+        Year: {movieDetails.Year} | Rating: {movieDetails.imdbRating} |
+        Language: {movieDetails.Language} | Genre: {movieDetails.Genre}
+      </h3>
+      <h4>Cast: {movieDetails.Actors}</h4>
+      <p>Plot: {movieDetails.Plot}</p>
+    </>
+  );
 }
 
 export default MovieDetails;
