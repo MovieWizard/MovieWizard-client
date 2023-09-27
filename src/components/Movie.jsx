f45f23e3d171ecf33d8979159e1b1da75e3000ec
Merna <merna.wahdan@gmail.com>
2023-09-28 01:22:19 +0200

    Handle movie click to show movie details

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Movie(props) {
+  const navigate = useNavigate();
   return (
-    <div className="movie-card">
+    <div
+      onClick={() => navigate(`/movies/${props._id}`)}
+      className="movie-card"
+    >
       <h3 className="movie-name">{props.title}</h3>
       <img className="movie-image" src={props.poster} alt="movie-image" />
     </div>
   );
-  
 }
 
 export default Movie;
