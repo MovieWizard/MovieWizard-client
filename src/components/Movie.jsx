49017713b07032c23c0334d25a9debaa626c42b9
Merna <merna.wahdan@gmail.com>
2023-09-28 20:26:24 +0200

    Handle broken posters & display "No Image" image

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import ImageWithFallback from "../components/NoImage";
 
 function Movie(props) {
   const navigate = useNavigate();
+
+  const handleMovieClick = () => {
+    navigate(`/movies/${props._id}`);
+  };
+
   return (
     <div
-      onClick={() => navigate(`/movies/${props._id}`)}
+      onClick={handleMovieClick}
       className={`movie-card ${props.disableHover ? "hover-disabled" : ""}`}
     >
       <h3 className="movie-name">{props.title}</h3>
-      <img className="movie-image" src={props.poster} alt="movie-image" />
+      <ImageWithFallback src={props.poster} alt="movie-image" />
     </div>
   );
 }
