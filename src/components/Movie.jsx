4517a41a09335a74e428f576ec19ca53e9e6f8a9
Merna <merna.wahdan@gmail.com>
2023-09-28 19:37:32 +0200

    Fix merge conflict in Movie model

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Movie(props) {
+  const navigate = useNavigate();
   return (
-    <div className={`movie-card ${props.disableHover ? "hover-disabled" : ""}`}>
+    <div
+      onClick={() => navigate(`/movies/${props._id}`)}
+      className={`movie-card ${props.disableHover ? "hover-disabled" : ""}`}
+    >
       <h3 className="movie-name">{props.title}</h3>
       <img className="movie-image" src={props.poster} alt="movie-image" />
     </div>
