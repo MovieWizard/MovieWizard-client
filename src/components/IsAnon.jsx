ea1f7399d80e98bd1b8fc76d5b00b9e16a9dc482
Beatriz <beatrizrmb@outlook.com>
2023-09-28 17:06:51 +0100

    Remove dark/light mode

--- src/components/IsAnon.jsx
+++ src/components/IsAnon.jsx
@@ -4,18 +4,23 @@ import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
 import { Navigate } from "react-router-dom";
 
-function IsAnon({children}) {
-    const { isLoggedIn, isLoading } = useContext(AuthContext);
+function IsAnon({ children }) {
+  const { isLoggedIn, isLoading } = useContext(AuthContext);
 
-    if (!isLoggedIn) 
-        return (
-    <>
-        <h1>Login</h1>
+  if (!isLoggedIn)
+    return (
+      <div className="LoginPage">
+        <h2>Login</h2>
 
         <form onSubmit={handleLoginSubmit}>
           <label>Email:</label>
-          <input type="email" name="email" value={email} onChange={handleEmail} />
-  
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleEmail}
+          />
+
           <label>Password:</label>
           <input
             type="password"
@@ -23,17 +28,17 @@ function IsAnon({children}) {
             value={password}
             onChange={handlePassword}
           />
-  
+
           <button type="submit">Login</button>
         </form>
-    </>
-    )
+      </div>
+    );
 
-    if (isLoggedIn) {
-        return <Navigate to="/"/>;
-    } else {
-        return children;
-    }
+  if (isLoggedIn) {
+    return <Navigate to="/" />;
+  } else {
+    return children;
+  }
 }
 
 export default IsAnon;
